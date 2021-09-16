e3c150bc909b63264c31d07ff13173306942cc8b
Jon Mereria <makgyber@gmail.com>
2021-09-16 23:28:03 +0800

    initial commit

---
+++ app/src/main/java/com/makgyber/panis/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.makgyber.panis
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnBarcode: Button
+    private lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        btnBarcode = findViewById(R.id.button)
+        textView = findViewById(R.id.txtContent)
+        btnBarcode.setOnClickListener {
+            val intentIntegrator = IntentIntegrator(this@MainActivity)
+            intentIntegrator.setBeepEnabled(false)
+            intentIntegrator.setCameraId(0)
+            intentIntegrator.setPrompt("SCAN")
+            intentIntegrator.setBarcodeImageEnabled(false)
+            intentIntegrator.initiateScan()
+        }
+    }
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("MainActivity", "Scanned")
+                Toast.makeText(this, "Scanned -> " + result.contents, Toast.LENGTH_SHORT)
+                    .show()
+                textView.text = String.format("Scanned Result: %s", result)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
